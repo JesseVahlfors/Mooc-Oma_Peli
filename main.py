287c74150dff1a782f9be25e87844f7497f66f98
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-12-16 22:25:18 +0200

    Refaktoroi hirviön toiminnallisuus luokkiin Hirvio ja PikkuHirvio
    - Siirrettiin yleiset hirviöön liittyvät ominaisuudet ja metodit Hirvio-luokkaan.
    - Luotiin PikkuHirvio-luokka, joka perii Hirvio-luokan, ja lisättiin siihen skaalattu hirviön kuva pienempää kokoa varten.
    - Parannettiin koodin rakenteellisuutta ja uudelleenkäytettävyyttä jakamalla toiminnallisuus selkeisiin luokkiin.
    - Valmisteltiin pohja uusille hirviötyypeille, jotka voivat polveutua Hirvio-luokasta.

--- main.py
+++ main.py
@@ -34,6 +34,73 @@ class RoboMiekka:
             
             return (miekan_alku_x , miekan_alku_y), (miekan_paa_x, miekan_paa_y)
         return None
+    
+class Hirvio:
+    def __init__(self, nayton_leveys, nayton_korkeus):
+        self.kuva = pygame.image.load("hirvio.png")
+        self.nopeus_x = 0
+        self.nopeus_y = 0
+        self.nayton_leveys = nayton_leveys
+        self.nayton_korkeus = nayton_korkeus
+        self.x, self.y = self.luo_aloituspaikka()
+
+    def luo_aloituspaikka(self):
+        #kuvan ulkopuoliset koordinaatit
+        vasen_x, oikea_x = -100, self.nayton_leveys + 100
+        yla_y, ala_y = -100, self.nayton_korkeus + 100
+
+        aloituspaikka = random.choice(["vasen", "oikea", "ylos", "alas"])
+        if aloituspaikka == "vasen":
+            x = random.randint(vasen_x, -20)
+            y = random.randint(0, self.nayton_korkeus - self.kuva.get_height())
+        elif aloituspaikka == "oikea":
+            x = random.randint(self.nayton_leveys + 20, oikea_x)
+            y = random.randint(0, self.nayton_korkeus - self.kuva.get_height())
+        elif aloituspaikka == "ylos":
+            x = random.randint(0, self.nayton_leveys - self.kuva.get_width())
+            y = random.randint(yla_y, -30)
+        else:
+            x = random.randint(0, self.nayton_leveys -1)
+            y = random.randint(self.nayton_korkeus +20, ala_y)
+
+        return x, y
+    
+    def hirvio_suunta(self, robo_keskipiste_x, robo_keskipiste_y):
+        hirvio_keskipiste_x = self.x + self.kuva.get_width()/2
+        hirvio_keskipiste_y = self.y + self.kuva.get_height()/2
+
+        if robo_keskipiste_x < hirvio_keskipiste_x:
+            self.nopeus_x = -1
+        elif robo_keskipiste_x > hirvio_keskipiste_x:
+            self.nopeus_x = 1
+        else:
+            self.nopeus_x = 0
+
+        if robo_keskipiste_y < hirvio_keskipiste_y:
+            self.nopeus_y = -1
+        elif robo_keskipiste_y > hirvio_keskipiste_y:
+            self.nopeus_y = 1
+        else:
+            self.nopeus_y = 0
+    
+    def liiku_hirvio(self):
+        self.x += self.nopeus_x
+        self.y += self.nopeus_y
+
+    def hirviot_paallekkain(self, muut_hirviot):
+        for muu_hirvio in muut_hirviot:
+            if self != muu_hirvio:
+                if (self.x <= muu_hirvio.x - 5 + muu_hirvio.kuva.get_width() and
+                    self.x + self.kuva.get_width() >= muu_hirvio.x and 
+                    self.y <= muu_hirvio.y - 5 + muu_hirvio.kuva.get_height() and
+                    self.y + self.kuva.get_height() >= muu_hirvio.y):
+                    return True
+        return False  
+
+class PikkuHirvio(Hirvio):
+    def __init__(self, nayton_leveys, nayton_korkeus):
+        super().__init__(nayton_leveys, nayton_korkeus)
+        self.kuva = pygame.transform.scale(self.kuva, (self.kuva.get_width() * 0.6, self.kuva.get_height() * 0.6))
 
 
 class Robo_Survivor:
@@ -62,43 +129,6 @@ class Robo_Survivor:
         self.robo_y = 240 - self.robo_kuva.get_height()/2
         self.robo_keskipiste_x = self.robo_x + self.robo_kuva.get_width()/2
         self.robo_keskipiste_y = self.robo_y + self.robo_kuva.get_height()/2
-
-
-    def hirvio(self):
-        x, y = self.luo_aloituspaikka()
-        kuva = pygame.image.load("hirvio.png")
-        hirvio_data = {
-        "kuva": pygame.transform.scale(kuva, (kuva.get_width() * 0.6, kuva.get_height() * 0.6)),
-        "x": x,
-        "y": y,  
-        "nopeus_x": 0,
-        "nopeus_y": 0,
-        }
-        return hirvio_data
-
-    def luo_aloituspaikka(self):
-        hirvio_kuva = pygame.image.load("hirvio.png")
-        #kuvan ulkopuoliset koordinaatit
-        vasen_x = -100
-        oikea_x = self.nayton_leveys + 100
-        yla_y = -100
-        ala_y = self.nayton_korkeus + 100
-
-        aloituspaikka = random.choice(["vasen", "oikea", "ylos", "alas"])
-        if aloituspaikka == "vasen":
-            x = random.randint(vasen_x, -20)
-            y = random.randint(0, self.nayton_korkeus - hirvio_kuva.get_height())
-        elif aloituspaikka == "oikea":
-            x = random.randint(self.nayton_leveys + 20, oikea_x)
-            y = random.randint(0, self.nayton_korkeus - hirvio_kuva.get_height())
-        elif aloituspaikka == "ylos":
-            x = random.randint(0, self.nayton_leveys -hirvio_kuva.get_width())
-            y = random.randint(yla_y, -30)
-        elif aloituspaikka == "alas":
-            x = random.randint(0, self.nayton_leveys -1)
-            y = random.randint(self.nayton_korkeus +20, ala_y)
-        return x, y
-    
 
     def tutki_tapahtumat(self):
         for tapahtuma in pygame.event.get():
@@ -134,65 +164,16 @@ class Robo_Survivor:
         self.robo_keskipiste_x = self.robo_x + self.robo_kuva.get_width()/2
         self.robo_keskipiste_y = self.robo_y + self.robo_kuva.get_height()/2
         
-
-    def hirvio_suunta(self, hirvio):
-        robo_keskipiste_x = self.robo_x + self.robo_kuva.get_width()/2
-        robo_keskipiste_y = self.robo_y + self.robo_kuva.get_height()/2
-        hirvio_keskipiste_x = hirvio["kuva"].get_width()/2
-        hirvio_keskipiste_y = hirvio["kuva"].get_height()/2
-        suunta_x = 0
-        suunta_y = 0
-
-        if robo_keskipiste_x < hirvio["x"] + hirvio_keskipiste_x:
-            suunta_x = -1
-        elif robo_keskipiste_x > hirvio["x"] + hirvio_keskipiste_x:
-            suunta_x = 1
-        else:
-            suunta_x = 0
-
-        if robo_keskipiste_y < hirvio["y"] + hirvio_keskipiste_y:
-            suunta_y = -1
-        elif robo_keskipiste_y > hirvio["y"] + hirvio_keskipiste_y:
-            suunta_y = 1
-        else:
-            suunta_y = 0
-
-        return suunta_x, suunta_y
-    
-    def liiku_hirvio(self, hirvio):
-        hirvio_x = hirvio["x"]
-        hirvio_y = hirvio["y"]
-        suunta_x, suunta_y  = self.hirvio_suunta(hirvio)
-        hirvio["x"] += suunta_x
-        hirvio["y"] += suunta_y
-
-        if self.hirviot_paallekkain(hirvio):
-            hirvio["x"] = hirvio_x
-            hirvio["y"] = hirvio_y
-            if hirvio["nopeus_x"] == 0 and hirvio["nopeus_y"] == 0:
-                hirvio["x"], hirvio["y"] = self.luo_aloituspaikka()
-
-
-
-
-    def hirviot_paallekkain(self, hirvio):
-            for muu_hirvio in self.hirviot:
-                if hirvio != muu_hirvio:
-                    if (hirvio["x"] <= muu_hirvio["x"] - 5 + muu_hirvio["kuva"].get_width() and
-                        hirvio["x"] + hirvio["kuva"].get_width() >= muu_hirvio["x"] and 
-                        hirvio["y"] <= muu_hirvio["y"] - 5 + muu_hirvio["kuva"].get_height() and
-                        hirvio["y"] + hirvio["kuva"].get_height() >= muu_hirvio["y"]):
-                        return True
-            return False
     
     def osuuko_roboon(self, hirvio):
-        uusi_x, uusi_y = self.luo_aloituspaikka()
-        if (hirvio["x"]<= self.robo_x + self.robo_kuva.get_width() and
-                hirvio["x"] + hirvio["kuva"].get_width() >= self.robo_x and 
-                hirvio["y"] <= self.robo_y + self.robo_kuva.get_height() and
-                hirvio["y"] + hirvio["kuva"].get_height() >= self.robo_y):
-                    hirvio["x"] = uusi_x
-                    hirvio["y"] = uusi_y
+        if (
+            hirvio.x <= self.robo_x + self.robo_kuva.get_width() and
+            hirvio.x + hirvio.kuva.get_width() >= self.robo_x and 
+            hirvio.y <= self.robo_y + self.robo_kuva.get_height() and
+            hirvio.y + hirvio.kuva.get_height() >= self.robo_y
+        ):
+            return True
+        return False
         
     def osuuko_hirvioon(self, miekan_isku, hirvio):
         if miekan_isku != None:
@@ -200,32 +181,38 @@ class Robo_Survivor:
             loppu_x, loppu_y = miekan_isku[1]
             keski_x = (alku_x + loppu_x) /2
             keski_y = (alku_y + loppu_y) /2
-            if (hirvio["x"] < loppu_x < hirvio["x"] + hirvio["kuva"].get_width() and
-                hirvio["y"] < loppu_y < hirvio["y"] + hirvio["kuva"].get_height()):
+            if (hirvio.x < loppu_x < hirvio.x + hirvio.kuva.get_width() and
+                hirvio.y < loppu_y < hirvio.y + hirvio.kuva.get_height()):
                 return True
-            if (hirvio["x"] < alku_x < hirvio["x"] + hirvio["kuva"].get_width() and
-                hirvio["y"] < alku_y < hirvio["y"] + hirvio["kuva"].get_height()):
+            if (hirvio.x < alku_x < hirvio.x + hirvio.kuva.get_width() and
+                hirvio.y < alku_y < hirvio.y + hirvio.kuva.get_height()):
                 return True
-            if (hirvio["x"] < keski_x < hirvio["x"] + hirvio["kuva"].get_width() and
-                hirvio["y"] < keski_y < hirvio["y"] + hirvio["kuva"].get_height()):
+            if (hirvio.x < keski_x < hirvio.x + hirvio.kuva.get_width() and
+                hirvio.y < keski_y < hirvio.y + hirvio.kuva.get_height()):
                 return True
         return False
     
     def silmukka(self):   
-        self.hirviot = [self.hirvio() for _ in range(15)]
+        self.hirviot = [PikkuHirvio(self.nayton_leveys, self.nayton_korkeus) for _ in range(15)]
         while True:
-            uusi_x, uusi_y = self.luo_aloituspaikka()
             self.tutki_tapahtumat()
             self.liiku_robo()
             self.naytto.fill((100,100,100))
             miekan_isku = self.robo_miekka.miekan_sijainti()
             for hirvio in self.hirviot:
-                self.liiku_hirvio(hirvio)
-                self.osuuko_roboon(hirvio)
+                hirvio.hirvio_suunta(self.robo_keskipiste_x, self.robo_keskipiste_y)
+                hirvio.liiku_hirvio()
+
+                if hirvio.hirviot_paallekkain(self.hirviot):
+                    hirvio.x, hirvio.y = hirvio.luo_aloituspaikka()
+
+                if self.osuuko_roboon(hirvio):
+                    hirvio.x, hirvio.y = hirvio.luo_aloituspaikka()
+                    
                 if self.osuuko_hirvioon(miekan_isku, hirvio):
-                    hirvio["x"] = uusi_x
-                    hirvio["y"] = uusi_y
-                self.naytto.blit(hirvio["kuva"], (hirvio["x"], hirvio["y"]))
+                    hirvio.x, hirvio.y = hirvio.luo_aloituspaikka()
+
+                self.naytto.blit(hirvio.kuva, (hirvio.x, hirvio.y))
                 
             self.robo_miekka.robo_sijainti(self.robo_keskipiste_x, self.robo_keskipiste_y)
             if miekan_isku:
